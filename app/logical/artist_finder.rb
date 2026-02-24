theclonedude/danbooru

commit 99416d47b743c88871ce12e06c91fb379ce2ee12

artists: fix slow dupe artist checks.
Fix issue where creating an artist could time out due to a slow query when checking for duplicate
artists.

app/logical/artist_finder.rb
@@ -24,7 +24,12 @@ module ArtistFinder
     url = url.downcase.gsub(%r{\Ahttps?://|/\z}, "") # "https://example.com/A/B/C/" => "example.com/a/b/c"
     while artists.empty? && url != "."
       u = url.gsub("*", '\*') + "/*"
-      artists += Artist.active.where(id: ArtistURL.normalized_url_like(u).select(:artist_id)).limit(10)
+
+      # XXX Using a CTE is a hack to force Postgres to use the regexp_replace(lower(url)) index instead of the artist_id index
+      # artists += Artist.active.where(id: ArtistURL.normalized_url_like(u).select(:artist_id).order(:url)).limit(10)
+      Artist.with(matching_artists: ArtistURL.select(:artist_id).normalized_url_like(u))
+            .joins("INNER JOIN matching_artists ON matching_artists.artist_id = artists.id")
+            .active.limit(10)
 
       # File.dirname("example.com/a/b/c") => "example.com/a/b"; File.dirname("example.com") => "."
       url = File.dirname(url)

commit 7f3ea12179d1ff9fbbf42dee312615ca22024670

artists: fixup artist finder regression in 99416d47b.

app/logical/artist_finder.rb
@@ -27,9 +27,9 @@ module ArtistFinder
 
       # XXX Using a CTE is a hack to force Postgres to use the regexp_replace(lower(url)) index instead of the artist_id index
       # artists += Artist.active.where(id: ArtistURL.normalized_url_like(u).select(:artist_id).order(:url)).limit(10)
-      Artist.with(matching_artists: ArtistURL.select(:artist_id).normalized_url_like(u))
-            .joins("INNER JOIN matching_artists ON matching_artists.artist_id = artists.id")
-            .active.limit(10)
+      artists += Artist.with(matching_artists: ArtistURL.select(:artist_id).normalized_url_like(u))
+                       .joins("INNER JOIN matching_artists ON matching_artists.artist_id = artists.id")
+                       .active.limit(10)
 
       # File.dirname("example.com/a/b/c") => "example.com/a/b"; File.dirname("example.com") => "."
       url = File.dirname(url)